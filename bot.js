b81294b6e4b90928de9105d67e16c603d545ffb3
zheerj <Zheer.j@yahoo.com>
2018-09-22 13:31:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === prefix + 'ping') {
-       const m = await message.channel.send("Ping?");
-       m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  	}
+  if(command === prefix + "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
